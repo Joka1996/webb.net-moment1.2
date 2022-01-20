a77519e1970a70e27f66e20d1263ad5837fcb364
Joel K <duveled@gmail.com>
2022-01-20 11:21:18 +0100

    Add project files.

---
+++ Pages/Razor.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MinWebbplats.Pages
+{
+    public class ContactModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ wwwroot/js/main.js
@@ -0,0 +1,9 @@
+﻿"use strict";
+
+console.log("hello from main.js");
+
+let footer = document.getElementById("footer");
+//byt färg på footer
+function changeColor() {
+    footer.style.backgroundColor = "yellow";
+}
